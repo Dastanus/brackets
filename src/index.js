46c47c5e5b46a3dbae6b768064dda8ac5e999144
Dastanus <nixe141417@gmail.com>
2019-02-20 23:22:23 +0300

    Completed task of Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let open = [], close = [], current = [], check = true;
+
+	bracketsConfig.forEach(item=>{
+		open.push(item[0]);
+		close.push(item[1]);
+	})
+
+	let arr = str.split("");
+	
+	for(let i=0; i<arr.length; i++){
+		if(open.includes(arr[i])){
+			current.push(arr[i])
+			if(current.length>1){
+				if((current[current.length-1]==current[current.length-2])&&(open.indexOf(current[current.length-1])==close.indexOf(current[current.length-1]))){
+					current.pop();
+					current.pop();
+				}
+			}
+		} else if (current.length==0){
+			check = false;
+		} else {
+			topCurrent = current.pop();
+			if(!equal(topCurrent,arr[i])){
+				check = false;
+			}
+		}
+
+		function equal(openSymbol, closeSymbol) {
+			return open.indexOf(openSymbol) == close.indexOf(closeSymbol);
+		}
+	}
+	if(check&&current.length==0){
+		return true;
+	} else {
+		return false;
+	}
 }
